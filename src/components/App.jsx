924f7ff795c5c059ca3ea8df98336cb494556ac7
markswinimer <markswinimer@gmail.com>
2020-05-06 13:27:02 -0400

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import styled from 'styled-components';
 
+const StyledApp = styled.div`
+	color: red;
+`
 export default class App extends React.Component {
 	render() {
 		return (
-			<div style={{ textAlign: "center" }}>
+			<StyledApp>
 				<h1>Hello World</h1>
-			</div>
+			</StyledApp>
 		);
 	}
 }
